023b331f838456903fa41357489fd1f9eaa9949d
alvyn le <alvyn279@gmail.com>
2020-10-24 20:03:26 -0400

    fix tests and added linter

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -2,20 +2,26 @@ import React from 'react';
 import logo from './utils/logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={'App'}>
+      <header className={'App-header'}>
+        <img
+          src={logo}
+          className={'App-logo'}
+          alt={'logo'}
+        />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to reload.
         </p>
-        <p>
-          This will be my new personal website
-        </p>
+        <p>This will be my new personal website</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
