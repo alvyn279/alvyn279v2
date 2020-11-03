8d806d60dca18d9295ca163a523798b577418b58
alvyn le <alvyn279@gmail.com>
2020-11-03 12:44:10 -0500

    populated timeline

--- website/src/data.ts
+++ website/src/data.ts
@@ -4,14 +4,58 @@ import { AboutProps } from './components/about/about';
 const TIMELINE_INFO: TimelineProps = {
   events: [
     {
-      dateString: 'Fall 2016',
+      dateString: 'Sep 2016',
       title: 'Student',
       institution: 'Concordia University',
       description: [
-        'Start of Software Engineering (BEng) degree at Concordia University. Enrolled in co-op.',
+        'Started Bachelor\'s of Software Engineering (BEng).',
+        'Enrolled in co-op.',
       ],
       dateStringBackgroundColor: '#800000',
       dateStringColor: '#FFD700',
+    }, {
+      dateString: 'Jan 2018 - Aug 2018',
+      title: 'Software Engineering Intern',
+      institution: 'Ericsson',
+      description: [
+        'Worked on a web application in Django.',
+        'Worked on a Java-based functional testing framework.',
+      ],
+      dateStringBackgroundColor: '#002561',
+      dateStringColor: '#FFFFFF',
+    }, {
+      dateString: 'May 2019 - Jan 2020',
+      title: 'Full-Stack Developer Intern',
+      institution: 'Ineat',
+      description: [
+        'Worked on a mobile application in React Native.',
+      ],
+      dateStringBackgroundColor: '#f31c4d',
+      dateStringColor: '#FFFFFF',
+    }, {
+      dateString: 'May 2020',
+      title: 'Student',
+      institution: 'Concordia University',
+      description: [
+        'Graduated Bachelor\'s of Software Engineering (BEng) with distinction.',
+      ],
+      dateStringBackgroundColor: '#800000',
+      dateStringColor: '#FFD700',
+    }, {
+      dateString: 'Jun 2020 - Sep 2020',
+      title: 'SDE Intern',
+      institution: 'Amazon Web Services (AWS)',
+      description: [
+        'Worked on a native AWS serverless web application.',
+      ],
+      dateStringBackgroundColor: '#232F3E',
+      dateStringColor: '#FF9900',
+    }, {
+      dateString: 'Feb 2021',
+      title: 'SDE',
+      institution: 'Amazon Web Services (AWS)',
+      dateStringBackgroundColor: '#232F3E',
+      dateStringColor: '#FF9900',
     },
   ],
 };
