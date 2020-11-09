387cad9a95db9b342bd0af56419ca72ee7488cc8
alvyn le <alvyn279@gmail.com>
2020-11-09 17:24:09 -0500

    Scaffold cdk app with creds and base stack

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,5 +3,13 @@ import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
 import { CdkStack } from '../lib/cdk-stack';
 
+const AWS_ACCOUNT = '459641237997';
+
 const app = new cdk.App();
-new CdkStack(app, 'S3ReactWebsite');
+
+new CdkStack(app, 'S3ReactPersonalWebsite', { 
+    env: { 
+      account: AWS_ACCOUNT, 
+      region: 'us-east-1',
+  }
+});
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,27 @@
 import cdk = require('@aws-cdk/core');
-import s3deploy= require('@aws-cdk/aws-s3-deployment');
-import s3 = require('@aws-cdk/aws-s3');
+import route53 = require('@aws-cdk/aws-route53');
 import { SPADeploy } from 'cdk-spa-deploy';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new SPADeploy(this, 'websiteDeploy')
-        .createBasicSite({
-          indexDoc: 'index.html',
-          websiteFolder: '../website/build'
-        })
+    const CUSTOM_DOMAIN_NAME: string = 'alvynle.me';
+
+    // Create HostedZone with custom domain name
+    const customHostedZone: route53.HostedZone = new route53.HostedZone(this, 'PersonalWebsiteHostedZone', {
+      zoneName: CUSTOM_DOMAIN_NAME,
+      comment: 'Change records on Namecheap if this HostedZone is replaced/deleted/.'
+    });
+
+    // Create deploy construct
+    new SPADeploy(this, 'PersonalWebsiteDeploy', {
+       encryptBucket: true 
+      })
+    .createSiteFromHostedZone({
+      zoneName: customHostedZone.zoneName,
+      indexDoc: 'index.html',
+      websiteFolder: '../website/build'
+    });
   }
 }
