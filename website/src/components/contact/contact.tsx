9669dbac9ce9b710b3aa09a15283248070311b2e
alvyn le <alvyn279@gmail.com>
2021-10-28 23:11:25 -0700

    feat: refactor intro and contact components

--- website/src/components/contact/contact.tsx
+++ website/src/components/contact/contact.tsx
@@ -8,105 +8,97 @@ import './contact.scss';
 import imageOverlay from '../../img/earth.jpg';
 
 interface ContactProps {
-  github: string,
-  linkedin: string,
-  email: string
+    github: string;
+    linkedin: string;
+    email: string;
 }
 
 const LOGO_PROPS = {
-  fontSize: '35px',
-  color: '#28384c',
-  style: {
-    height: '100%',
-  },
+    fontSize: '35px',
+    color: '#28384c',
+    style: {
+        height: '100%',
+    },
 };
 
-class Contact extends React.Component<ContactProps> {
-  render() {
-    const { github, linkedin, email } = this.props;
+const Contact = (props: ContactProps) => {
+    const { github, linkedin, email } = props;
+
+    const renderSocialRow = () => (
+        <div className={'row'}>
+            <div className={'contact-content'}>
+                <div className={'more-info'}>
+                    <p className={'lead'}>
+                        Feel free to reach out to me for any inquiry.
+                    </p>
+                </div>
+                <div className={'socials'}>
+                    <ul>
+                        <li>
+                            <SocialLink
+                                link={github}
+                                size={45}
+                                logo={<LogoGithub {...LOGO_PROPS} />}
+                            />
+                        </li>
+                        <li>
+                            <SocialLink
+                                link={linkedin}
+                                size={45}
+                                logo={<LogoLinkedin {...LOGO_PROPS} />}
+                            />
+                        </li>
+                        <li>
+                            <SocialLink
+                                link={`mailto:${email}`}
+                                size={45}
+                                logo={<IosMail {...LOGO_PROPS} />}
+                            />
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 
     return (
-      <section
-        className={'paralax-mf footer-paralax bg-image sect-mt4 route'}
-        style={{ backgroundImage: `url(${imageOverlay})` }}
-      >
-        <div className={'overlay-mf'} />
-        <div className={'container'}>
-          <div className={'row'}>
-            <div className={'col-sm-12'}>
-              <div className={'contact-mf'}>
-                <div
-                  id={'contact'}
-                  className={'box-shadow-full big-card'}
-                >
-                  <div className={'row'}>
-                    <div className={'title-box-2 pt-4 pt-md-0'}>
-                      <h5 className={'title-left'}>Contact Me</h5>
+        <section
+            className={'paralax-mf footer-paralax bg-image sect-mt4 route'}
+            style={{ backgroundImage: `url(${imageOverlay})` }}
+        >
+            <div className={'overlay-mf'} />
+            <div className={'container'}>
+                <div className={'row'}>
+                    <div className={'col-sm-12'}>
+                        <div className={'contact-mf'}>
+                            <div
+                                id={'contact'}
+                                className={'box-shadow-full big-card'}
+                            >
+                                <div className={'row'}>
+                                    <div className={'title-box-2 pt-4 pt-md-0'}>
+                                        <h5 className={'title-left'}>
+                                            Contact Me
+                                        </h5>
+                                    </div>
+                                </div>
+                                {renderSocialRow()}
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                  <div className={'row'}>
-                    <div className={'contact-content'}>
-                      <div className={'more-info'}>
-                        <p className={'lead'}>
-                          Feel free to reach out to me for any inquiry.
-                        </p>
-                      </div>
-                      <div className={'socials'}>
-                        <ul>
-                          <li>
-                            <SocialLink
-                              link={github}
-                              size={45}
-                              logo={(
-                                <LogoGithub
-                                  {...LOGO_PROPS}
-                                />
-                              )}
-                            />
-                          </li>
-                          <li>
-                            <SocialLink
-                              link={linkedin}
-                              size={45}
-                              logo={(
-                                <LogoLinkedin
-                                  {...LOGO_PROPS}
-                                />
-                              )}
-                            />
-                          </li>
-                          <li>
-                            <SocialLink
-                              link={`mailto:${email}`}
-                              size={45}
-                              logo={(
-                                <IosMail
-                                  {...LOGO_PROPS}
-                                />
-                              )}
-                            />
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
                 </div>
-              </div>
             </div>
-          </div>
-        </div>
-        <footer>
-          <div className={'container'}>
-            <div className={'row'}>
-              <div className={'col-sm-12'}>
-                <div className={'copyright-box'} />
-              </div>
-            </div>
-          </div>
-        </footer>
-      </section>
+            <footer>
+                <div className={'container'}>
+                    <div className={'row'}>
+                        <div className={'col-sm-12'}>
+                            <div className={'copyright-box'} />
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </section>
     );
-  }
-}
+};
 
 export default Contact;
--- website/src/components/intro/intro.tsx
+++ website/src/components/intro/intro.tsx
@@ -3,58 +3,51 @@ import './stars.scss';
 import Typed from 'react-typed';
 import ScrollLink from '../scroll-link/scroll-link';
 
-const HIGHLIGHT_TITLES = [
-  'Software Engineer',
-  'Sports Fan',
-  'Stargazer',
-];
+const HIGHLIGHT_TITLES = ['Software Engineer', 'Sports Fan', 'Stargazer'];
 
-class Intro extends React.Component {
-  render() {
-    return (
-      <div
-        id={'home'}
-        className={'intro route bg-image background'}
-      >
+const Intro = () => (
+    <div id={'home'} className={'intro route bg-image background'}>
         <div id={'stars'} />
         <div id={'stars2'} />
         <div id={'stars3'} />
 
         <div className={'intro-content display-table'}>
-          <div className={'table-cell'}>
-            <div className={'container'}>
-              <h1 className={'intro-title mb-4'}>Bonjour/Hi, I&apos;m Alvyn.</h1>
-              <p className={'intro-subtitle'}>
-                <span className={'text-slider-items'} />
-                <strong className={'text-slider'}>
-                  <Typed
-                    strings={HIGHLIGHT_TITLES}
-                    typeSpeed={80}
-                    backDelay={1100}
-                    backSpeed={30}
-                    loop
-                  />
-                </strong>
-              </p>
-              <p className={'pt-3'}>
-                <ScrollLink
-                  to={'work'}
-                  title={(
-                    <span
-                      className={'btn btn-primary btn js-scroll px-4'}
-                      role={'button'}
-                    >
-                      View My Work
-                    </span>
-                  )}
-                />
-              </p>
+            <div className={'table-cell'}>
+                <div className={'container'}>
+                    <h1 className={'intro-title mb-4'}>
+                        Bonjour/Hi, I&apos;m Alvyn.
+                    </h1>
+                    <p className={'intro-subtitle'}>
+                        <span className={'text-slider-items'} />
+                        <strong className={'text-slider'}>
+                            <Typed
+                                strings={HIGHLIGHT_TITLES}
+                                typeSpeed={80}
+                                backDelay={1100}
+                                backSpeed={30}
+                                loop
+                            />
+                        </strong>
+                    </p>
+                    <p className={'pt-3'}>
+                        <ScrollLink
+                            to={'work'}
+                            title={
+                                <span
+                                    className={
+                                        'btn btn-primary btn js-scroll px-4'
+                                    }
+                                    role={'button'}
+                                >
+                                    View My Work
+                                </span>
+                            }
+                        />
+                    </p>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    );
-  }
-}
+    </div>
+);
 
 export default Intro;
